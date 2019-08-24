e81dc587545ac42166d2ff77ec6de651c6c775d4
Raman Goyal <ramaniitrgoyal92@tamu.edu>
2019-08-24 16:20:52 -0500

    Update Main_Example_class_k_Dbar_Dynamics.m

--- Software_Verification_and_Examples/Dynamics_Examples/02_Example_Class_K_Dbar_Dynamics/Main_Example_class_k_Dbar_Dynamics.m
+++ Software_Verification_and_Examples/Dynamics_Examples/02_Example_Class_K_Dbar_Dynamics/Main_Example_class_k_Dbar_Dynamics.m
@@ -3,7 +3,7 @@
 % * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 
 % EXAMPLE:
-clear all; clc; close all;
+clear all; clc; close all; warning off;
 
 % Specify initial node positions for class k structure
 
