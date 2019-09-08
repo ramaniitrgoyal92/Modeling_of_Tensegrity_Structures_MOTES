f8979f87c3fb1d4fa5d9d6e0a808dc36836586e8
Raman Goyal <ramaniitrgoyal92@tamu.edu>
2019-09-08 10:32:19 -0500

    Update Main_Example_String_Only_Dynamics.m

--- Software_Verification_and_Examples/Dynamics_Examples/01_Example_Class_0_String_Only_Dynamics/Main_Example_String_Only_Dynamics.m
+++ Software_Verification_and_Examples/Dynamics_Examples/01_Example_Class_0_String_Only_Dynamics/Main_Example_String_Only_Dynamics.m
@@ -1,9 +1,9 @@
 %% Example: String_Only_Dynamics
-
+%
 % /* This Source Code Form is subject to the terms of the Mozilla Public
 % * License, v. 2.0. If a copy of the MPL was not distributed with this
 % * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
-
+%
 % EXAMPLE:
 %    - Manual node matrix specification
 %    - Manual connectivity matrix generation
