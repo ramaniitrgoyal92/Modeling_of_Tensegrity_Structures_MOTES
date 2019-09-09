a726717c6e3f61d816f033b75eec94f92e2a2836
Raman Goyal <ramaniitrgoyal92@tamu.edu>
2019-09-09 11:23:25 -0500

    Update setup.m

--- setup.m
+++ setup.m
@@ -1,6 +1,5 @@
 %% ^_^ Welcome to Modeling of Tensegrity Structrues(MOTES) software! ^_^ %%
-
-%% SETUP file to be run only the first time
+% SETUP file to be run only the first time
 
 % /* This Source Code Form is subject to the terms of the Mozilla Public
 % * License, v. 2.0. If a copy of the MPL was not distributed with this
